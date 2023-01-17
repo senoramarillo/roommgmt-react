b7e6de38063dfa783d61c6a93a8c20e0c8965322
Anonymous <anonymous@ano.com>
2023-01-17 01:30:35 +0100

    update SwaggerConfig

--- src/main/java/de/dlh/lhind/exercise/roommgmt/configuration/SwaggerConfig.java
+++ src/main/java/de/dlh/lhind/exercise/roommgmt/configuration/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
     }
 
     @Bean
-    public HttpTraceRepository htttpTraceRepository() {
+    public HttpTraceRepository httpTraceRepository() {
         return new InMemoryHttpTraceRepository();
     }
 
